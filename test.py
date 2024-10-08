0fc77af132b3ec89cf6b7aeea6ec92c42d81c611
uzevie George <uzeviegeorge@gmail.com>
2024-10-08 20:15:23 +0100

    i made a few changes

---
+++ test.py
@@ -0,0 +1,45 @@
+
+
+
+
+# username = "ade"
+# password = "1234"
+
+# while True:
+#     check_username = input("please enter your username ")
+#     check_password = input("please enter your password ")
+#     if check_username == username and check_password == password:
+#         print("verification successful!")
+#         break
+#     else:
+#         print("""
+# you are wrong try again!
+# you are wrong try again!
+# you are wrong try again!
+# you are wrong try again!
+# you are wrong try again!
+#         """)
+
+
+secret_num = 43
+guess_limit = 5
+guess_count = 0
+users_inputs = []
+has_user_guessed = False
+print("you have 5 chances to guess the secret number!")
+while guess_count < guess_limit:
+    number = int(input("guess the sercet number: "))
+    guess_count += 1
+    has_user_guessed = True
+    chances = guess_limit - guess_count
+    users_inputs.append(number)
+    
+    if number == secret_num:
+        print("congrats you got the number!")
+        break
+    if has_user_guessed and chances != 0:
+    	print(f"try again you have {chances} chance(s) left")    
+else:
+    print("sorry you failed!")
+    
+    print(f'here are what you guessed {users_inputs} but the secret number was "{secret_num}"')
